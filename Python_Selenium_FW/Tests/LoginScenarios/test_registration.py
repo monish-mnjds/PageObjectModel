59929a41df6a5d03aea2091e9c6afe5ab85d9399
Monish <66905892+monish-mnjds@users.noreply.github.com>
2021-03-14 22:47:15 +0530

    Add files via upload

---
+++ Python_Selenium_FW/Tests/LoginScenarios/test_registration.py
@@ -0,0 +1,20 @@
+from Tests import *
+from POM import *
+
+headers, data = read_data("Shopping", "test_registration")
+
+
+@pytest.mark.parametrize(headers, data)
+@pytest.mark.usefixtures("init")
+class TestRegistration:
+    def test_registration(self, fname, lname, email, password):
+        rp = RegistrationPage(self.driver)
+        rp.click_register()
+        rp.select_male()
+        rp.enter_firstname(fname)
+        rp.enter_lastname(lname)
+        rp.enter_email(email)
+        rp.enter_password(password)
+        rp.enter_confirm_password(password)
+        rp.click_register_button()
+        rp.verify_registration_successful()
